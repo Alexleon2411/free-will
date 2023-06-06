c4967eeecef300d19325cea6011b1eb9777bf94a
mustanser ali <razaali199913@outlook.com>
2023-06-06 15:06:21 +0200

    fixed typos

--- app/controllers/propositions_controller.rb
+++ app/controllers/propositions_controller.rb
@@ -1,15 +1,15 @@
 class PropositionsController < ApplicationController
   before_action :set_event, only: [:destroy, :update, :edit]
   def new
-    @preposition = Event.new
+    @proposition = Event.new
   end
 
   def create
-    @preposition = Preposition.new(preposition_params)
+    @proposition = Proposition.new(proposition_params)
     @user = current_user
-    @preposition.user = @user
-    if @preposition.save
-      redirect_to prepositions_path
+    @proposition.user = @user
+    if @proposition.save
+      redirect_to propositions_path
     else
       render :new, status: :unprocessable_entity
     end
@@ -19,25 +19,25 @@ class PropositionsController < ApplicationController
   end
 
   def update
-    if @preposition.update(preposition_params)
-      redirect_to preposition_path(@preposition)
+    if @proposition.update(proposition_params)
+      redirect_to proposition_path(@proposition)
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @preposition.destroy
-    redirect_to prepositions_path, status: :see_other
+    @proposition.destroy
+    redirect_to propositions_path, status: :see_other
   end
 
   private
 
   def set_event
-    @preposition = Preposition.find(params[:id])
+    @proposition = Proposition.find(params[:id])
   end
 
-  def preposition_params
-    params.require(:preposition).permit(:content)
+  def proposition_params
+    params.require(:proposition).permit(:content)
   end
 end
