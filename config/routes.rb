bad020b618f534b45150b443b802045db9f153e5
mustanser ali <razaali199913@outlook.com>
2023-06-06 14:30:13 +0200

    added routes validations associations and actions for events and prepositions

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :events do
+    resources :propsitions, except: [:index, :show]
+  end
 end
