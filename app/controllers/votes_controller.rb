7e5e9ebf48711f585ce89e45d9b619f490fb770a
mustanser ali <razaali199913@outlook.com>
2023-06-07 14:24:22 +0200

    pull

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,10 @@
 class VotesController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
-    @preposition = Preposition.find(params[:proposition_id])
+    @proposition = Proposition.find(params[:proposition_id])
     @vote = Vote.new
     @vote.event = @event
-    @vote.proposition = @prepositiion
+    @vote.proposition = @proposition
     @vote.save
   end
 end
