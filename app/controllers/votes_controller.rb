0a72c54e3a5c3806d4888ff342d6e8684c744661
Alejandro Leon <79841123+Alexleon2411@users.noreply.github.com>
2023-06-07 14:41:51 +0200

    now we can vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,11 @@
 class VotesController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
-    @preposition = Preposition.find(params[:proposition_id])
+    @proposition = Proposition.find(params[:proposition_id])
     @vote = Vote.new
-    @vote.event = @event
-    @vote.proposition = @prepositiion
-    @vote.save
+    @vote.proposition = @proposition
+    @vote.proposition.event = @event
+    @vote.user = current_user
+    @vote.save!
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,6 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :participants
   has_many :propositions, dependent: :destroy
+  has_many :votes, through: :propositions
   validates :name, presence: true
 end
