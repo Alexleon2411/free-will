b9a737f885d230e3fd3c723b7472b50398482e60
mustanser ali <razaali199913@outlook.com>
2023-06-12 12:28:25 +0200

    destroy action in votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -9,4 +9,15 @@ class VotesController < ApplicationController
     @vote.save!
     redirect_to event_path(@event)
   end
+  def update
+    @vote = Vote.update
+  end
+  def destroy
+    @user_vote = Event.find(params[:event_id]).votes.where(user: User.current_user)
+    if @user_vote.exists?
+      @user_vote.destroy
+    else
+      render :create
+    end
+  end
 end
